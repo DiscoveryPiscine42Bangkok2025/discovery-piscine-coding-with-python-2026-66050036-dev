df16379e00077035fffed570d4296707822608ad
66050036-dev <66050036@kmitl.ac.th>
2026-02-10 20:37:33 +0700

    Add files via upload

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print("Enter a number less than 25")
+try:
+    number = int(input())
+
+    if number > 25:
+        print("Error")
+    else:
+        for i in range(number, 26):
+            print(f"Inside the loop, my variable is {i}")
+
+except ValueError:
+    pass
