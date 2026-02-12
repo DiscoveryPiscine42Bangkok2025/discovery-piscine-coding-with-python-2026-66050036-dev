c5ef572f308f38c20067e43e555b66ac3f2edb79
66050036-dev <66050036@kmitl.ac.th>
2026-02-13 00:22:49 +0700

    Add files via upload

---
+++ Rush/main.py
@@ -0,0 +1,21 @@
+from checkmate import checkmate
+
+
+def main():
+    board = """\
+R...
+.K..
+..P.
+...."""
+
+    result = checkmate(board)
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
+
+#R...
+#.K..
+#..P.
+#...."""
